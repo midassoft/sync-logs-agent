b7e6167adad4d65a2da6f8922a1a9b0955963011
ChristianBatista <elcatchon@gmail.com>
2025-03-31 06:42:01 -0400

    Cleaning repository - removing all files

--- app/api/endpoints/logs.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter()
-
-@router.post("/api/logs")
-async def receive_log(data: dict):
-    print(data)
-    return {"status": "success", "message": "Log received"}
--- app/core/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-class Settings:
-    LOG_STORAGE = os.getenv("LOG_STORAGE", "file")  # "file" o "database"
-
-settings = Settings()
